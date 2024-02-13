c539cd847bfd5b41e511cb31f2fda2827498f050
dedomil <dedomil@skiff.com>
2024-02-13 21:01:23 +0530

    create a struct to get exact size of url instead of hardcoding it as 200 eariler

--- rust-program/programs/url-shortener/src/lib.rs
+++ rust-program/programs/url-shortener/src/lib.rs
@@ -6,26 +6,30 @@ declare_id!("5evXQyffaUhYwCKGg7SoTZTfdDXCwKaQogTYQ873vHmg");
 mod url_shortener {
     use super::*;
 
-    pub fn create(ctx: Context<Create>, url: String) -> Result<()> {
+    pub fn create(ctx: Context<Create>, args: CreateUrlAccountArgs) -> Result<()> {
         let url_account: &mut Account<UrlAccount> = &mut ctx.accounts.url_account;
 
-        if url.chars().count() < 1 {
+        if args.url.chars().count() < 1 {
             return err!(Errors::EmptyURL);
         }
 
         url_account.authority = ctx.accounts.signer.key();
-        url_account.url = url;
-        url_account.shortcode = Clock::get().unwrap().unix_timestamp.to_string();
+        url_account.url = args.url;
+        url_account.shortcode = Clock::get().unwrap().unix_timestamp;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
+#[instruction(args: CreateUrlAccountArgs)]
 pub struct Create<'info> {
     #[account(
         init,
         payer = signer,
-        space = 32 + 5 * 4 + 200 + 8
+        space = 8 // discriminator 8
+            + 32 // authority 40
+            + 8 // shortcode 48
+            + 4 + args.url.len() // 4 + bytes | 52+
     )]
     pub url_account: Account<'info, UrlAccount>,
     #[account(mut)]
@@ -35,13 +39,20 @@ pub struct Create<'info> {
 
 #[account]
 pub struct UrlAccount {
-    authority: Pubkey,
-    shortcode: String,
-    url: String,
+    // discriminator   // 8 = 8
+    authority: Pubkey, // 32 = 40
+    shortcode: i64,    // 8 = 48
+    // fields below cannot be accessed by `getProgramAccounts` since they are of variable size
+    url: String, // 4+
 }
 
 #[error_code]
 pub enum Errors {
     #[msg("URL cannot be empty")]
     EmptyURL,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct CreateUrlAccountArgs {
+    url: String,
 }
